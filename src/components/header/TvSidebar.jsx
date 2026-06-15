37a1ffa639bb45806f0612e50eacfe7f0caa19bb
tung le <82189441+TungLT159@users.noreply.github.com>
2026-06-15 15:38:55 +0700

    chore: cleanup TvSidebar unused useEffect import

--- src/components/header/TvSidebar.jsx
+++ src/components/header/TvSidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './tv-sidebar.scss';
 import logo from '../../assets/logo.png';
